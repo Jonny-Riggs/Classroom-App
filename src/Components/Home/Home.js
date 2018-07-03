87bd4a4c452e8e3219b7e8b29b09952e8ed45aab
Jonny-Riggs <jonny.riggs615@gmail.com>
2018-07-03 14:44:13 -0500

    final commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,28 +10,37 @@ export default class Home extends Component {
             <article className="tile is-child box is-info">
               <p className="title">Hi! My Name Is Mrs. Riggs</p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                semper diam at erat pulvinar, at pulvinar felis blandit.
-                Vestibulum volutpat tellus diam, consequat gravida libero
-                rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc
-                dui porta orci, quis semper odio felis ut quam.
+                At Dan Mills, we are focusing on developing strong literacy and
+                numeracy skills; providing an affirming, safe environment for
+                children; using data to inform instruction; and implementing
+                engaging and challenging enrichment opportunities for all
+                students. Our theme is MAKING EACH DAY COUNT. The theme was
+                created to hold all stakeholders accountable for the academic
+                and social growth we expect daily at our school. Every second of
+                every day is focused on promoting success.
               </p>
             </article>
             <div className="tile is-child box">
               <p className="title">Education</p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-                ornare magna eros, eu pellentesque tortor vestibulum ut.
-                Maecenas non massa sem. Etiam finibus odio quis feugiat
-                facilisis.
+                Encourage children to talk about reading and writing experiences.
+                Provide many opportunities for children to explore and identify
+                sound-symbol relationships in meaningful contexts. Help children
+                to segment spoken words into individual sounds and blend the
+                sounds into whole words (for example, by slowly writing a word
+                and saying its sound). Frequently read interesting and
+                conceptually rich stories to children. Provide daily
+                opportunities for children to write. Help children build a sight
+                vocabulary. Create a literacy-rich environment for children to
+                engage independently in reading and writing.
               </p>
             </div>
           </div>
-            <div className="tile is-child">
+          <div className="tile is-child">
             <figure className="image is-1by1">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRljZ4yhHZ0d6KKx6rhNfKRbbAr22603xhN3WvTrpQTQU2WHQMzhA"/>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRljZ4yhHZ0d6KKx6rhNfKRbbAr22603xhN3WvTrpQTQU2WHQMzhA" />
             </figure>
-            </div>
+          </div>
           <div className="tile is-parent">
             <div className="tile is-child box">
               <p className="title">What to Expect!</p>
@@ -65,8 +74,8 @@ export default class Home extends Component {
             <div className="content has-text-centered">
               <p>
                 <strong>Mrs. Riggs Classroom</strong> by{' '}
-                  Jonathan Riggs
                 <a href="https://github.com/Jonny-Riggs/Classroom-App">
+                  Jonathan Riggs
                 </a>
               </p>
             </div>
